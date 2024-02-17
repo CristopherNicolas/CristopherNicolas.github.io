30945bfae8d43a849bb31306a95c85a1be760dfb
CristopherNicolas <cristopher.faundez@uniacc.edu>
2024-02-17 18:22:32 -0300

    movilk

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ function ChangeVideo()
     var vid = document.getElementById('background-video');
     vid.src = 'resources/bgVid.mp4';
 }
+
+if(esNavegadorMovil())
+{
+    alert("estas en un navegador movil");
+}
+function esNavegadorMovil() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
