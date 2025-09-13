ea08fd0eb721655b9d500653c42c01afc48aad51
ccubenexus <cristopher.faundez@uniacc.edu>
2025-09-13 03:48:21 -0300

    updateembed

--- src/components/Casino/blackjack.jsx
+++ src/components/Casino/blackjack.jsx
@@ -10,7 +10,7 @@ const Blackjack = () => {
       <h1 className="blackjack-title">Blackjack</h1>
 
       <div className="blackjack-embed-wrapper">
-      <iframe frameBorder="0" src="https://itch.io/embed-upload/14944038?color=086a00" allowfullscreen="" width="1300" height="760"><a href="https://criztophernicolas.itch.io/blackjack">Play blackjack on itch.io</a></iframe>
+     <iframe frameBorder="0" src="https://itch.io/embed-upload/14944524?color=086a00" allowfullscreen="" width="1300" height="760"><a href="https://criztophernicolas.itch.io/blackjack">Play blackjack on itch.io</a></iframe>
       </div>
       <br></br>
        <button onClick={() => navigate("/")}>
